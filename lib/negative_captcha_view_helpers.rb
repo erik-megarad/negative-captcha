330d206eab208b9f0bf7bffda23fb32dd70603de
Sean Roberts <roberts.sean@gmail.com>
2008-10-27 16:57:36 -0400

    Changed view helpers to work properly with HAML

--- lib/negative_captcha_view_helpers.rb
+++ lib/negative_captcha_view_helpers.rb
@@ -7,17 +7,17 @@ module NegativeCaptchaViewHelpers
   end
   
   def negative_text_field_tag(negative_captcha, field, options={})
-    text_field_tag(negative_captcha.fields[field], negative_captcha.values[field], options.merge(:tabindex => '1')) +
-    content_tag('div', :style => 'position: absolute; left: -2000px;') {
-      text_field_tag(field, '', :tabindex => '999', :autocomplete => 'off')
-    }
+    [
+      text_field_tag(negative_captcha.fields[field], negative_captcha.values[field], options.merge(:tabindex => '1')),
+      "<div style='position: absolute; left: -2000px;'>" + text_field_tag(field, '', :tabindex => '999', :autocomplete => 'off') + "</div>"
+    ].join
   end
   
   def negative_text_area_tag(negative_captcha, field, options={})
-    text_area_tag(negative_captcha.fields[field], negative_captcha.values[field], options.merge(:tabindex => '1')) +
-    content_tag('div', :style => 'position: absolute; left: -2000px;') {
-      text_area_tag(field, '', :tabindex => '999', :autocomplete => 'off')
-    }
+    [
+      text_area_tag(negative_captcha.fields[field], negative_captcha.values[field] || options[:value], options.merge(:tabindex => '1')),
+      "<div style='position: absolute; left: -2000px;'>" + text_area_tag(field, '', :tabindex => '999', :autocomplete => 'off') + "</div>"
+    ].join
   end
   
   #TODO: Select, check_box, etc
