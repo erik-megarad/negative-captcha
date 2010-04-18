c417644bcaab9cdcf688b5f5f68809bbd21a22cf
Maksym Kammerer <mak_sym@interia.pl>
2010-04-18 21:18:31 +0200

    Added view helpers - negative_password_field_tag and negative_hidden_field_tag

--- lib/negative_captcha_view_helpers.rb
+++ lib/negative_captcha_view_helpers.rb
@@ -19,6 +19,21 @@ module NegativeCaptchaViewHelpers
       "<div style='position: absolute; left: -2000px;'>" + text_area_tag(field, '', :tabindex => '999', :rows => 2, :cols => 1) + "</div>"
     ].join
   end
+
+  def negative_hidden_field_tag(negative_captcha, field, options={})
+    [
+      hidden_field_tag(negative_captcha.fields[field], negative_captcha.values[field], options),
+      "<div style='position: absolute; left: -2000px;'>" + hidden_field_tag(field, '', :tabindex => '999') + "</div>"
+    ].join
+  end
+
+  def negative_password_field_tag(negative_captcha, field, options={})
+    [
+      password_field_tag(negative_captcha.fields[field], negative_captcha.values[field], options),
+      "<div style='position: absolute; left: -2000px;'>" + password_field_tag(field, '', :tabindex => '999') + "</div>"
+    ].join
+  end
+  
   def negative_label_tag(negative_captcha, field, name, options={})
     [
       label_tag(negative_captcha.fields[field], name, options)
