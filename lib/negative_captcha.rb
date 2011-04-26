866ed632eff6df1f4438900e3ed25b69a4baa9bd
Erik Peterson <erik@subwindow.com>
2011-04-26 16:15:56 -0400

    Fix tests in case opts doesn't have params

--- lib/negative_captcha.rb
+++ lib/negative_captcha.rb
@@ -8,10 +8,10 @@ class NegativeCaptcha
   attr_accessor :message
   attr_accessor :timestamp
   attr_accessor :error
-  
+
   def initialize(opts)
     @secret = opts[:secret]||MD5.hexdigest("this_is_a_secret_key")
-    @timestamp = opts[:params][:timestamp] || Time.now()
+    @timestamp =  (opts.has_key?(:params) ? opts[:params][:timestamp] : nil) || Time.now()
     @spinner = MD5.hexdigest(([@timestamp, @secret] + (opts[:spinner].is_a?(Array) ? opts[:spinner] : [opts[:spinner]]))*'-')
     @message = opts[:message]||"Please try again. This usually happens because an automated script attempted to submit this form."
     @fields = opts[:fields].inject({}){|hash, field_name| hash[field_name] = MD5.hexdigest([field_name, @spinner, @secret]*'-'); hash }
@@ -19,15 +19,15 @@ class NegativeCaptcha
     @error = "No params provided"
     process(opts[:params]) if opts[:params] && (opts[:params][:spinner]||opts[:params][:timestamp])
   end
-  
+
   def [](name)
     @fields[name]
   end
-  
+
   def valid?
     @error.blank?
   end
-  
+
   def process(params)
     if params[:timestamp].nil? || (Time.now.to_i - params[:timestamp].to_i).abs > 1.day.to_i
       @error = "Error: Invalid timestamp.  #{@message}"
