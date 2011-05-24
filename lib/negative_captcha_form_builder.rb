5e3c009673a90db55f83e21edfc4eb4c5dca4a48
stefan <stefantseng@gmail.com>
2011-05-23 22:39:04 -0300

    Removed deprecation warnings

--- lib/negative_captcha_form_builder.rb
+++ lib/negative_captcha_form_builder.rb
@@ -3,13 +3,13 @@ module ActionView
     class FormBuilder
       def negative_text_field(captcha, method, options = {})
         html = @template.negative_text_field_tag(captcha, method, options).html_safe
-        html = "<div class='fieldWithErrors'>#{html}</div>" if @object.errors.on(method).present?
+        html = "<div class='fieldWithErrors'>#{html}</div>" if @object.errors[method].present?
         html.html_safe
       end 
 
       def negative_text_area(captcha, method, options = {})
         html = @template.negative_text_area_tag(captcha, method, options).html_safe
-        html = "<div class='fieldWithErrors'>#{html}</div>" if @object.errors.on(method).present?
+        html = "<div class='fieldWithErrors'>#{html}</div>" if @object.errors[method].present?
         html.html_safe
       end
 
@@ -19,13 +19,13 @@ module ActionView
 
       def negative_password_field(captcha, method, options = {})
         html = @template.negative_password_field_tag(captcha, method, options).html_safe
-        html = "<div class='fieldWithErrors'>#{html}</div>" if @object.errors.on(method).present?
+        html = "<div class='fieldWithErrors'>#{html}</div>" if @object.errors[method].present?
         html.html_safe
       end
 
       def negative_label(captcha, method, name, options = {})
         html = @template.negative_label_tag(captcha, method, name, options).html_safe
-        html = "<div class='fieldWithErrors'>#{html}</div>" if @object.errors.on(method).present?
+        html = "<div class='fieldWithErrors'>#{html}</div>" if @object.errors[method].present?
         html.html_safe
       end
     end
