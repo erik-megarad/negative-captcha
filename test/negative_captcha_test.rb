34b4415188b1c330844857a4a1df6d20682935df
Michael Pearson <mipearson@gmail.com>
2013-06-17 12:13:34 +1000

    Test that negative captcha returns field data as it is encrypted

--- test/negative_captcha_test.rb
+++ test/negative_captcha_test.rb
@@ -3,8 +3,18 @@ require 'test/unit'
 require_relative '../lib/negative_captcha'
 
 class NegativeCaptchaTest < Test::Unit::TestCase
+
   def test_view_helpers
     assert ActionView::Base.instance_methods.include?(:negative_captcha)
+  end
+
+  def encrypted_params captcha, fields=nil
+    params = {}
+    fields ||= captcha.fields.keys
+    fields.each do |field|
+      params[captcha.fields[field]] = field.to_s
+    end
+    params
   end
 
   def test_valid_submission
@@ -16,10 +26,12 @@ class NegativeCaptchaTest < Test::Unit::TestCase
     filled_form = NegativeCaptcha.new(
       :fields => fields,
       :timestamp => captcha.timestamp,
-      :params => {:timestamp => captcha.timestamp, :spinner => captcha.spinner}.merge(captcha.fields.inject({}){|hash, name, encrypted_name| hash[encrypted_name] = name; hash})
+      :params => {:timestamp => captcha.timestamp, :spinner => captcha.spinner}.merge(encrypted_params(captcha))
     )
     assert_equal "", filled_form.error
     assert filled_form.valid?
+    assert_equal 'name', filled_form.values[:name]
+    assert_equal 'comment', filled_form.values[:comment]
   end
 
   def test_missing_timestamp
@@ -31,7 +43,7 @@ class NegativeCaptchaTest < Test::Unit::TestCase
     filled_form = NegativeCaptcha.new(
       :fields => fields,
       :timestamp => captcha.timestamp,
-      :params => {:spinner => captcha.spinner}.merge(captcha.fields.inject({}){|hash, name, encrypted_name| hash[encrypted_name] = name; hash})
+      :params => {:spinner => captcha.spinner}.merge(encrypted_params(captcha))
     )
     assert !filled_form.valid?
     assert filled_form.error.match(/timestamp/).is_a?(MatchData)
@@ -46,7 +58,7 @@ class NegativeCaptchaTest < Test::Unit::TestCase
     filled_form = NegativeCaptcha.new(
       :fields => fields,
       :timestamp => captcha.timestamp,
-      :params => {:timestamp => 1209600, :spinner => captcha.spinner}.merge(captcha.fields.inject({}){|hash, name, encrypted_name| hash[encrypted_name] = name; hash})
+      :params => {:timestamp => 1209600, :spinner => captcha.spinner}.merge(encrypted_params(captcha))
     )
     assert !filled_form.valid?
     assert filled_form.error.match(/timestamp/).is_a?(MatchData)
@@ -61,7 +73,7 @@ class NegativeCaptchaTest < Test::Unit::TestCase
     filled_form = NegativeCaptcha.new(
       :fields => fields,
       :timestamp => captcha.timestamp,
-      :params => {:timestamp => captcha.timestamp}.merge(captcha.fields.inject({}){|hash, name, encrypted_name| hash[encrypted_name] = name; hash})
+      :params => {:timestamp => captcha.timestamp}.merge(encrypted_params(captcha))
     )
     assert !filled_form.valid?
     assert filled_form.error.match(/spinner/).is_a?(MatchData)
@@ -76,7 +88,7 @@ class NegativeCaptchaTest < Test::Unit::TestCase
     filled_form = NegativeCaptcha.new(
       :fields => fields,
       :timestamp => captcha.timestamp,
-      :params => {:timestamp => captcha.timestamp, :spinner => captcha.spinner.reverse}.merge(captcha.fields.inject({}){|hash, name, encrypted_name| hash[encrypted_name] = name; hash})
+      :params => {:timestamp => captcha.timestamp, :spinner => captcha.spinner.reverse}.merge(encrypted_params(captcha))
     )
     assert !filled_form.valid?
     assert filled_form.error.match(/spinner/).is_a?(MatchData)
@@ -91,7 +103,7 @@ class NegativeCaptchaTest < Test::Unit::TestCase
     filled_form = NegativeCaptcha.new(
       :fields => fields,
       :timestamp => captcha.timestamp,
-      :params => {:timestamp => captcha.timestamp, :spinner => captcha.spinner, :name => "Test"}.merge(captcha.fields.inject({}){|hash, name, encrypted_name| hash[encrypted_name] = name; hash})
+      :params => {:timestamp => captcha.timestamp, :spinner => captcha.spinner, :name => "Test"}.merge(encrypted_params(captcha))
     )
     assert !filled_form.valid?
     assert filled_form.error.match(/hidden/i).is_a?(MatchData)
@@ -104,7 +116,7 @@ class NegativeCaptchaTest < Test::Unit::TestCase
     filled_form = NegativeCaptcha.new(
       :fields => fields,
       :timestamp => captcha.timestamp,
-      :params => {:timestamp => captcha.timestamp, :spinner => captcha.spinner, :one => ' ', :two => "\r\n", :three => "\n"}.merge(captcha.fields.inject({}){|hash, name, encrypted_name| hash[encrypted_name] = name; hash})
+      :params => {:timestamp => captcha.timestamp, :spinner => captcha.spinner, :one => ' ', :two => "\r\n", :three => "\n"}.merge(encrypted_params(captcha))
     )
     assert_equal "", filled_form.error
     assert filled_form.valid?
